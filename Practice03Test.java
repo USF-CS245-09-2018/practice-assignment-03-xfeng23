15f0aaa07466557227916fcaf2b03af13a5b8b96
XueFeng <xfeng23@usfca.edu>
2018-09-08 00:54:29 -0700

    hw3.v2

--- Practice03Test.java
+++ Practice03Test.java
@@ -1,17 +1,16 @@
 import java.util.Arrays;
 import java.util.Random;
 
-
 public class Practice03Test {
 
 	protected int count = 0;
-	protected double [] arr;
+	protected double[] arr;
 	private int resIndex = -1;
-
+	
 	/**
 	 * Constructor
 	 */
-	public Practice03Test (String [] args) {
+	public Practice03Test(String[] args) {
 		try {
 			count = Integer.parseInt(args[0]);
 		} catch (Exception e) {
@@ -22,10 +21,9 @@ public class Practice03Test {
 		generate_array();
 	}
 
-
 	/**
-	 * print_array: prints the array of doubles... formatted so it fits
-	 * ... on many small screens.
+	 * print_array: prints the array of doubles... formatted so it fits ... on many
+	 * small screens.
 	 */
 	public void print_array() {
 		System.out.println("------------------------------------");
@@ -38,7 +36,6 @@ public class Practice03Test {
 		System.out.println("\n------------------------------------");
 	}
 
-
 	/**
 	 * Fills the array with random double instances.
 	 */
@@ -48,64 +45,77 @@ public class Practice03Test {
 		double max = 100.0;
 
 		for (int i = 0; i < arr.length; i++) {
-			arr[i] = min + rand.nextDouble() * (max-min);
+			arr[i] = min + rand.nextDouble() * (max - min);
 		}
 	}
 
-
-	public int find_min_iterative () {
+	public int find_min_iterative() {
 		// TODO: Fill in this iterative function.
 		int count = 0;
-		for(int i = 1; i < arr.length; i++) {
-			if(arr[i] < arr[count]) {
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] < arr[count]) {
 				count = i;
 			}
 		}
 		return count;
 	}
 
-
-	public int find_min_recursive () {
-		// TODO: Fill in this recursive function.
-		
-		find_min_recursive(0, arr);
+//	public int find_min_recursive () {
+//		// TODO: Fill in this recursive function.
+//		
+//		find_min_recursive(0, arr);
+//		return resIndex;
+//	
+//	}
+//	
+//	public double find_min_recursive (int index, double[] array) {
+//		
+//		if (index == array.length-1) {
+//			resIndex = index;
+//			return array[index];
+//		}
+//			
+//		double tempRes = find_min_recursive(index+1, array);
+//		if (array[index] < tempRes) {
+//			resIndex = index;
+//			return array[index];
+//		}
+//		
+//		else {
+//			return tempRes;
+//		}
+//	}
+//	
+	public int find_min_recursive() {
+		int maxValue = Integer.MAX_VALUE;
+		find_min_recurisive(arr, 0, maxValue);
 		return resIndex;
-	
-	}
-	
-	public double find_min_recursive (int index, double[] array) {
-		
-		if (index == array.length-1) {
-			resIndex = index;
-			return array[index];
-		}
-			
-		double tempRes = find_min_recursive(index+1, array);
-		if (array[index] < tempRes) {
-			resIndex = index;
-			return array[index];
-		}
-		
-		else {
-			return tempRes;
-		}
 	}
 
+	public void find_min_recurisive(double[] array, int index, double maxValue) {
+
+		// return case
+		if (index == array.length) return;
+		else if (array[index] < maxValue) {
+			resIndex = index;
+			find_min_recurisive(array, index + 1, array[resIndex]);
+		}
+		else find_min_recurisive(array, index + 1, maxValue);
+	}
 
 	/**
-	 * print_min: determines the min iteratively and recursively.
-	 * ... and prints them both.
+	 * print_min: determines the min iteratively and recursively. ... and prints
+	 * them both.
 	 */
 	public void print_min() {
 		System.out.println("Iteratively determined min at index " + find_min_iterative());
 		System.out.println("Recursively determined min at index " + find_min_recursive());
 	}
 
-
 	/**
 	 * main for Practice 03: print the array and determine the min.
 	 */
-	public static void main(String [] args) {
+	public static void main(String[] args) {
 		Practice03Test test = new Practice03Test(args);
 		test.print_array();
 		test.print_min();
